b61bcf775b2ff6a2f41a9381b6559af4151e5603
Jan Hybš <x3mSpeedy@gmail.com>
2012-10-19 12:03:29 +0200

    Tests changed

--- src/edu/x3m/kas/core/Main.java
+++ src/edu/x3m/kas/core/Main.java
@@ -1,8 +1,7 @@
 package edu.x3m.kas.core;
 
 
-
-
+import edu.x3m.kas.monitors.IHuffmannMonitor;
 import edu.x3m.kas.utils.Timer;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,21 +21,22 @@ public class Main {
     public static void main (String[] args) throws FileNotFoundException, IOException, InterruptedException, UnsupportedFileException {
 
 
-        File f1 = new File ("./testfiles/test-01");
-        File f2 = new File ("./testfiles/test-01.x3m.huff");
-        //File f1 = new File ("./testfiles/screenshots.bmp");
-        //File f2 = new File ("./testfiles/screenshots.bmp.x3m.huff");
-        //File f1 = new File ("./testfiles/club-bleach.gif");
-        //File f2 = new File ("./testfiles/club-bleach.gif.x3m.huff");
-        //File f1 = new File ("./testfiles/trololo.flv");
-        //File f2 = new File ("./testfiles/trololo.flv.x3m.huff");
-        //File f1 = new File ("./testfiles/galaxy.mp4");
-        //File f2 = new File ("./testfiles/galaxy.mp4.x3m.huff");
+        //File f1 = new File ("C:/huff/testfiles/test-01");
+        //File f2 = new File ("C:/huff/testfiles/test-01.x3m.huff");
+        File f1 = new File ("C:/huff/testfiles/screenshots.bmp");
+        File f2 = new File ("C:/huff/testfiles/screenshots.bmp.x3m.huff");
+        //File f1 = new File ("C:/huff/testfiles/club-bleach.gif");
+        //File f2 = new File ("C:/huff/testfiles/club-bleach.gif.x3m.huff");
+        //File f1 = new File ("C:/huff/testfiles/trololo.flv");
+        //File f2 = new File ("C:/huff/testfiles/trololo.flv.x3m.huff");
+        //File f1 = new File ("C:/huff/testfiles/galaxy.mp4");
+        //File f2 = new File ("C:/huff/testfiles/galaxy.mp4.x3m.huff");
 
 
 
         TIMER.tic ();
         HuffmannEncoder h = new HuffmannEncoder (f1);
+        h.setHuffmannMonitor (new ProgressMonitor ());
         h.encode ();
         TIMER.tac ("Encoded: ");
 
@@ -45,7 +45,32 @@ public class Main {
 
         TIMER.tic ();
         HuffmannDecoder d = new HuffmannDecoder (f2);
+        d.setHuffmannMonitor (new ProgressMonitor ());
         d.decode ();
         TIMER.tac ("Decoded: ");
+    }
+
+
+    static class ProgressMonitor implements IHuffmannMonitor {
+
+
+        @Override
+        public void onSectionStart (String section) {
+            System.out.println ("start   : " + section);
+        }
+
+
+
+        @Override
+        public void onSectionEnd (String section) {
+            System.out.println ("end     : " + section);
+        }
+
+
+
+        @Override
+        public void onSectionProgress (String section, int prc) {
+            System.out.println ("progress: " + section + " (" + prc + "%)");
+        }
     }
 }
